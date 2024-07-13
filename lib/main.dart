356c21486a41da603a2db9bf84c82dff8592f696
Bullets-jyr <jyr@bullets.co.kr>
2024-07-13 15:59:00 +0900

    http 패키지 학습 (실습)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MainApp());
@@ -10,10 +13,136 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String api = 'https://jsonplaceholder.typicode.com/posts';
+  String responseText = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: () async {
+                  final response = await http.get(Uri.parse(api));
+                  if (response.statusCode == 200) {
+                    setState(() {
+                      responseText = response.body;
+                    });
+                  }
+                },
+                child: Text(
+                  'Http Get',
+                ),
+              ),
+              TextButton(
+                onPressed: () async {
+                  final response = await http.post(
+                    Uri.parse(api),
+                    headers: {
+                      'content-type': 'application/json; charset=UTF-8'
+                    },
+                    body: jsonEncode(
+                      {
+                        'title': 'foo',
+                        'body': 'bar',
+                        'userId': 1,
+                      },
+                    ),
+                  );
+                  print(response.statusCode);
+                  if (response.statusCode == 201) {
+                    setState(() {
+                      responseText = response.body;
+                    });
+                  }
+                },
+                child: Text(
+                  'Http Post',
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  http.put(
+                    Uri.parse('$api/1'),
+                    headers: {
+                      'content-type': 'application/json; charset=UTF-8'
+                    },
+                    body: jsonEncode(
+                      {
+                        'title': 'foo',
+                        'body': 'bar',
+                        'userId': 1,
+                      },
+                    ),
+                  ).then((value) {
+                    print(value.statusCode);
+                    setState(() {
+                      responseText = value.body;
+                    });
+                  });
+                },
+                child: Text(
+                  'Http Put',
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  http.delete(
+                    Uri.parse('$api/1'),
+                    // headers: {
+                    //   'content-type': 'application/json; charset=UTF-8'
+                    // },
+                    // body: jsonEncode(
+                    //   {
+                    //     'title': 'foo',
+                    //     'body': 'bar',
+                    //     'userId': 1,
+                    //   },
+                    // ),
+                  ).then((response) {
+                    print(response.statusCode);
+                    if (response.statusCode == 200) {
+                      setState(() {
+                        responseText = response.body;
+                      });
+                    }
+                  });
+                },
+                child: Text(
+                  'Http Delete',
+                ),
+              ),
+              Divider(),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Text(
+                    responseText,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
       ),
     );
   }
